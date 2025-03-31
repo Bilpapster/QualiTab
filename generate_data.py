1c228b5698dd160d8bdea65b687179cae1459b10
Vasileios Papastergios <papastva@csd.auth.gr>
2025-03-31 15:13:37 +0300

    Extract random seed to a variable and access generated data

--- generate_data.py
+++ generate_data.py
@@ -7,6 +7,8 @@ from sklearn.model_selection import train_test_split
 
 from tabpfn_extensions import TabPFNClassifier, unsupervised
 
+RANDOM_SEED = 42
+
 # Load the breast cancer dataset
 df = load_breast_cancer(return_X_y=False)
 X, y = df["data"], df["target"]
@@ -17,7 +19,7 @@ X_train, X_test, y_train, y_test = train_test_split(
     X,
     y,
     test_size=0.5,
-    random_state=42,
+    random_state=RANDOM_SEED,
 )
 
 # Initialize TabPFN models
@@ -47,21 +49,27 @@ exp_synthetic = unsupervised.experiments.GenerateSyntheticDataExperiment(
 X_tensor = torch.tensor(X_train, dtype=torch.float32)
 y_tensor = torch.tensor(y_train, dtype=torch.float32)
 
-# print(X_tensor)
-
 # Run the experiment
 results = exp_synthetic.run(
     tabpfn=model_unsupervised,
     X=X_tensor,
     y=y_tensor,
     attribute_names=attribute_names,
-    temp=2.0,
+    temp=1.0,
     n_samples=X_train.shape[0] * 3,  # Generate 3x original samples
     indices=feature_indices,
 )
+
+print(f"{X_train.shape[0]}*{3}={X_train.shape[0]*3}")
 
 import matplotlib.pyplot as plt
 
 plt.show()
 
-# print(results)
+print("Generated data (real and synthetic):")
+import pandas as pd
+pd.set_option('display.max_columns', None)
+pd.set_option('display.width', 1000)
+with pd.option_context("display.max_rows", None, "display.max_columns", None):
+    # display(exp_synthetic.data)
+    print(exp_synthetic.data) # exp_synthetic.data contains
