8ab659973f2b18b0445518462bb06dd0152b6dff
Vasileios Papastergios <papastva@csd.auth.gr>
2025-04-27 11:24:41 +0300

    Use the newly introduced improved classes for OpenML experiments

--- src/main.py
+++ src/main.py
@@ -1,5 +1,12 @@
-from experiment import CleanML_classification_experiment, OpenML_classification_experiment
+from experiment import CleanML_classification_experiment, OpenML_classification_experiment, OpenMLClassificationExperimentImproved
+from config import openML_dataset_configs
+from utils import get_seeds_from_env_or_else_default
+
 
 if __name__ == "__main__":
     # CleanML_classification_experiment().run()
-    OpenML_classification_experiment().run()
+    # OpenML_classification_experiment().run()
+    OpenMLClassificationExperimentImproved(
+        benchmark_configs=openML_dataset_configs,
+        random_seeds=get_seeds_from_env_or_else_default()
+    ).run()
