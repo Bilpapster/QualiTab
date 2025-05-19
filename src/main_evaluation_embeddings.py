0bf5367399f9ed168d707a3050462e4f3e09941f
Vasileios Papastergios <papastva@csd.auth.gr>
2025-05-19 09:19:46 +0300

    Retain a temporary replica of the old embeddings evaluation script for double checking completeness

--- src/main_evaluation_embeddings.py
+++ src/main_evaluation_embeddings.py
@@ -1,10 +1,10 @@
 import psycopg2
 import pandas as pd
 import numpy as np
-from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 from sklearn.cluster import KMeans
 from sklearn.metrics import roc_auc_score, accuracy_score
+from sklearn.neighbors import NearestNeighbors
 import json
 from uuid import uuid4
 import openml
@@ -13,6 +13,8 @@ from utils import connect_to_db, get_openML_task_mapping
 # Table names
 EXPERIMENTS_TABLE = "embeddings_experiments"
 EVALUATION_TABLE = "embedding_evaluation_metrics"
+BATCH_SIZE = 300
+K_NEIGHBORS_VALUES = [1, 3, 5, 10]
 
 def calculate_purity(labels_true, labels_pred):
     """Calculates the purity score for clustering."""
@@ -25,15 +27,32 @@ def calculate_purity(labels_true, labels_pred):
         cluster_purity.append(purity)
     return np.mean(cluster_purity)
 
+
+def calculate_knn_similarity(embeddings, k_values):
+    """Calculates the average cosine similarity to the k-nearest neighbors."""
+    knn_results = {}
+    for k in k_values:
+        if len(embeddings) < k + 1:
+            print(f"Warning: Not enough samples for k={k}. Skipping.")
+            continue
+        knn = NearestNeighbors(n_neighbors=k + 1, metric='cosine')  # +1 to exclude self
+        knn.fit(embeddings)
+        distances, _ = knn.kneighbors(embeddings)
+        # Average distance to the k nearest neighbors (excluding self)
+        avg_similarity = np.mean(1 - distances[:, 1:])  # Convert distance to similarity
+        knn_results[f'Avg Cosine Similarity (k={k})'] = avg_similarity
+    return knn_results
+
+
 def evaluate_embeddings(experiment):
     """
-    Evaluates the embeddings from a single experiment using linear probing and clustering.
+    Evaluates the embeddings from a single experiment using linear probing, clustering, and KNN.
 
     Args:
         experiment (dict): A dictionary representing a row from the embeddings_experiments table.
 
     Returns:
-        dict: A dictionary containing the evaluation metrics.
+        list: A list of dictionaries containing the evaluation metrics.
     """
     from experiment.OpenMLExperiment import OpenMLExperiment
 
@@ -43,119 +62,259 @@ def evaluate_embeddings(experiment):
     random_seed = experiment['random_seed']
     test_embeddings_list = experiment['test_embeddings']
     number_of_classes = experiment['number_of_classes']
+    corrupted_rows = experiment['corrupted_rows']
+    tag = experiment['tag']
 
-    print(type(test_embeddings_list))
+    # if corrupted rows is a nested list, keep only the second element
+    if isinstance(corrupted_rows, list) and len(corrupted_rows) > 0 and isinstance(corrupted_rows[0], list):
+        print(f"Corrupted rows is a nested list. Unpacking it.")
+        corrupted_rows = corrupted_rows[1]
 
     if not test_embeddings_list:
         print(f"Warning: No test embeddings found for experiment {experiment_id}")
         return []
 
-    # test_embeddings_list = json.loads(test_embeddings_json)
-    test_embeddings = np.array([np.array(emb) for emb in test_embeddings_list])
-    # exit()
+    test_embeddings = np.array(test_embeddings_list)
 
-    # Assuming you have a way to retrieve the true labels for the test set
-    # This might involve querying another table or storing them in the experiments table
-    # For now, let's assume 'true_labels' is available in the experiment dictionary
+    # Retrieve data from OpenML and reproduce transformations and split
     task_to_dataset_id_mapping = get_openML_task_mapping()
     experiment_object = OpenMLExperiment()
     experiment_object.random_seed = random_seed
     experiment_object.task = openml.tasks.get_task(task_to_dataset_id_mapping[dataset_id])
-    experiment_object.load_dataset(dataset_config=dict()) # uses default train test split (70/30)
+    experiment_object.load_dataset(dataset_config=dict())
+
+    for corrupted_row in corrupted_rows:
+        if not corrupted_row in experiment_object.X_test.index.tolist():
+            print(f"Warning: Some corrupted rows are not in the test set. The experiment is not reproducible.")
+            # write the dataset id to a file
+            with open("datasets_to_repeat.txt", "a") as f:
+                f.write(f"{dataset_name}\n")
+            return []
+
+    # Convert corrupted_rows to a list of locations instead of indices
+    corrupted_rows = [experiment_object.X_test.index.get_loc(row) for row in corrupted_rows]
 
     true_labels = np.array(experiment_object.y_test)
 
     evaluation_results = []
 
+    if test_embeddings.shape[0] == 2*len(true_labels):
+        # keep only the first half of the test embeddings
+        test_embeddings = test_embeddings[:len(true_labels)]
+
+
     if test_embeddings.shape[0] == len(true_labels) and number_of_classes > 1:
-        # Linear Probing (Logistic Regression)
+        # Linear Probing
         try:
-            # X_train, X_test, y_train, y_test = train_test_split(test_embeddings, true_labels, test_size=0.2, random_state=42)
-            logistic_model = LogisticRegression(random_state=random_seed, solver='liblinear', multi_class='ovr', max_iter=1000)
-            # logistic_model.fit(X_train, y_train)
-            # y_pred_proba = logistic_model.predict_proba(X_test)
+            logistic_model = LogisticRegression(random_state=random_seed, solver='liblinear', max_iter=1000)
             logistic_model.fit(test_embeddings, experiment_object.y_test)
             y_pred_proba = logistic_model.predict_proba(test_embeddings)
-
-            # Handle binary vs. multi-class
-            if number_of_classes == 2:
-                # roc_auc = roc_auc_score(y_test, y_pred_proba[:, 1])
-                roc_auc = roc_auc_score(experiment_object.y_test, y_pred_proba[:, 1])
-            else:
-                # roc_auc = roc_auc_score(y_test, y_pred_proba, multi_class='ovr')
-                roc_auc = roc_auc_score(experiment_object.y_test, y_pred_proba, multi_class='ovr')
-
+            roc_auc = roc_auc_score(true_labels, y_pred_proba, multi_class='ovr') if number_of_classes > 2 else roc_auc_score(true_labels, y_pred_proba[:, 1])
             evaluation_results.append({
-                'evaluation_type': 'linear probing',
+                'evaluation_type': 'linear probing fit to all',
                 'metric_name': 'ROC AUC',
                 'metric_value': roc_auc,
                 'weights': json.dumps(logistic_model.coef_.tolist()) if hasattr(logistic_model, 'coef_') else None
             })
         except ValueError as e:
-            print(f"Error during linear probing for experiment {experiment_id}: {e}")
+            print(f"Error during linear probing (all) for experiment {experiment_id}: {e}")
+
+        if corrupted_rows:
+            # we have to find the unaffected rows: all the rest values in the test set
+            unaffected_rows = [i for i in range(len(test_embeddings)) if i not in corrupted_rows]
+
+            test_embeddings_corrupted = test_embeddings[corrupted_rows]
+            y_test_corrupted = experiment_object.y_test.iloc[corrupted_rows]
+            test_embeddings_unaffected = test_embeddings[unaffected_rows]
+            y_test_unaffected = experiment_object.y_test.iloc[unaffected_rows]
+            if len(test_embeddings_corrupted) != len(y_test_corrupted):
+                print(f"Warning: The number of corrupted rows does not match the number of test embeddings.")
+                print(f"Number of corrupted rows: {len(test_embeddings_corrupted)}")
+                print(f"Number of corrupted test embeddings: {len(y_test_corrupted)}")
+                exit()
+            if len(test_embeddings_unaffected) != len(y_test_unaffected):
+                print(f"Warning: The number of unaffected rows does not match the number of test embeddings.")
+                print(f"Number of unaffected rows: {len(test_embeddings_unaffected)}")
+                print(f"Number of unaffected test embeddings: {len(y_test_unaffected)}")
+                exit()
+
+            try:
+                # Execute fine-grained linear probing: fit on unaffected, predict on corrupted
+                logistic_model = LogisticRegression(random_state=random_seed, solver='liblinear', max_iter=1000)
+                logistic_model.fit(test_embeddings_unaffected, y_test_unaffected)
+                y_pred_proba = logistic_model.predict_proba(test_embeddings_corrupted)
+                roc_auc = roc_auc_score(y_test_corrupted, y_pred_proba, multi_class='ovr') if number_of_classes > 2 else roc_auc_score(y_test_corrupted, y_pred_proba[:, 1])
+                evaluation_results.append({
+                    'evaluation_type': 'linear probing unaffected-corrupted',
+                    'metric_name': 'ROC AUC',
+                    'metric_value': roc_auc,
+                    'weights': json.dumps(logistic_model.coef_.tolist()) if hasattr(logistic_model, 'coef_') else None
+                })
+            except ValueError as e:
+                print(f"Error during linear probing (unaffected-corrupted) for experiment {experiment_id}: {e}")
+
+            try:
+                # Execute fine-grained linear probing: fit on corrupted, predict on unaffected
+                logistic_model = LogisticRegression(random_state=random_seed, solver='liblinear', max_iter=1000)
+                logistic_model.fit(test_embeddings_corrupted, y_test_corrupted)
+                y_pred_proba = logistic_model.predict_proba(test_embeddings_unaffected)
+                roc_auc = roc_auc_score(y_test_unaffected, y_pred_proba, multi_class='ovr') if number_of_classes > 2 else roc_auc_score(y_test_unaffected, y_pred_proba[:, 1])
+                evaluation_results.append({
+                    'evaluation_type': 'linear probing corrupted-unaffected',
+                    'metric_name': 'ROC AUC',
+                    'metric_value': roc_auc,
+                    'weights': json.dumps(logistic_model.coef_.tolist()) if hasattr(logistic_model, 'coef_') else None
+                })
+            except ValueError as e:
+                print(f"Error during linear probing (corrupted-unaffected) for experiment {experiment_id}: {e}")
+
+            try:
+                # Execute fine-grained k-means only on unaffected
+                kmeans_model = KMeans(n_clusters=number_of_classes, random_state=42, n_init='auto')
+                cluster_labels = kmeans_model.fit_predict(test_embeddings_unaffected)
+                purity = calculate_purity(y_test_unaffected, cluster_labels)
+                evaluation_results.append({
+                    'evaluation_type': 'clustering unaffected',
+                    'metric_name': 'Purity',
+                    'metric_value': purity,
+                    'weights': json.dumps(kmeans_model.cluster_centers_.tolist()) if hasattr(kmeans_model,
+                                                                                             'cluster_centers_') else None
+                })
+            except ValueError as e:
+                print(f"Error during clustering for experiment {experiment_id}: {e}")
+
+            try:
+                # Execute fine-grained k-means only on corrupted
+                kmeans_model = KMeans(n_clusters=number_of_classes, random_state=42, n_init='auto')
+                cluster_labels = kmeans_model.fit_predict(test_embeddings_corrupted)
+                purity = calculate_purity(y_test_corrupted, cluster_labels)
+                evaluation_results.append({
+                    'evaluation_type': 'clustering corrupted',
+                    'metric_name': 'Purity',
+                    'metric_value': purity,
+                    'weights': json.dumps(kmeans_model.cluster_centers_.tolist()) if hasattr(kmeans_model,
+                                                                                             'cluster_centers_') else None
+                })
+            except ValueError as e:
+                print(f"Error during clustering for experiment {experiment_id}: {e}")
+
+            # K-Nearest Neighbors Similarity
+            knn_results = calculate_knn_similarity(test_embeddings_unaffected, K_NEIGHBORS_VALUES)
+            for metric_name, metric_value in knn_results.items():
+                evaluation_results.append({
+                    'evaluation_type': 'knn similarity',
+                    'metric_name': metric_name + " (unaffected)",
+                    'metric_value': metric_value,
+                    'weights': None
+                })
+
+            # K-Nearest Neighbors Similarity
+            knn_results = calculate_knn_similarity(test_embeddings_corrupted, K_NEIGHBORS_VALUES)
+            for metric_name, metric_value in knn_results.items():
+                evaluation_results.append({
+                    'evaluation_type': 'knn similarity',
+                    'metric_name': metric_name + " (corrupted)",
+                    'metric_value': metric_value,
+                    'weights': None
+                })
+
+
 
         # Clustering (K-Means)
         try:
+            # Execute k-means clustering on the entire test set
             kmeans_model = KMeans(n_clusters=number_of_classes, random_state=42, n_init='auto')
             cluster_labels = kmeans_model.fit_predict(test_embeddings)
             purity = calculate_purity(true_labels, cluster_labels)
-
             evaluation_results.append({
-                'evaluation_type': 'clustering',
+                'evaluation_type': 'clustering all test',
                 'metric_name': 'Purity',
                 'metric_value': purity,
                 'weights': json.dumps(kmeans_model.cluster_centers_.tolist()) if hasattr(kmeans_model, 'cluster_centers_') else None
             })
         except ValueError as e:
             print(f"Error during clustering for experiment {experiment_id}: {e}")
+
+        # K-Nearest Neighbors Similarity
+        knn_results = calculate_knn_similarity(test_embeddings, K_NEIGHBORS_VALUES)
+        for metric_name, metric_value in knn_results.items():
+            evaluation_results.append({
+                'evaluation_type': 'knn similarity',
+                'metric_name': metric_name + " (all)",
+                'metric_value': metric_value,
+                'weights': None
+            })
+
     else:
         print(f"Warning: Inconsistent data or single class for experiment {experiment_id}, skipping evaluation.")
+        print(f"------------- Shape of test embeddings: {test_embeddings.shape} -------------")
+        print(f"------------- Shape of true labels: {len(true_labels)} -------------")
+        print(f"------------- Number of classes: {number_of_classes} -------------")
+        print(f"------------- Experiment ID: {experiment_id} -------------")
+        print(f"------------- Shape of X_train: {experiment_object.X_train.shape} -------------")
+        print(f"------------- Shape of y_train: {experiment_object.y_train.shape} -------------")
+        print(f"------------- Shape of X_test: {experiment_object.X_test.shape} -------------")
+        print(f"------------- Shape of y_test: {experiment_object.y_test.shape} -------------")
+        print()
 
     return evaluation_results
 
-def store_evaluation_metrics(conn, results, experiment_id):
-    """Stores the evaluation metrics in the embedding_evaluation_metrics table."""
+def store_evaluation_metrics(conn, result, experiment_id):
+    """Stores a single evaluation metric in the embedding_evaluation_metrics table."""
     cursor = conn.cursor()
-    for result in results:
-        evaluation_id = uuid4()
-        sql = f"""
-            INSERT INTO {EVALUATION_TABLE} (evaluation_id, experiment_id, evaluation_type, metric_name, metric_value, weights)
-            VALUES (%s, %s, %s, %s, %s, %s);
-        """
-        cursor.execute(sql, (str(evaluation_id), str(experiment_id), result['evaluation_type'], result['metric_name'], result['metric_value'], result.get('weights')))
+    evaluation_id = uuid4()
+    sql = f"""
+        INSERT INTO {EVALUATION_TABLE} (evaluation_id, experiment_id, evaluation_type, metric_name, metric_value, weights)
+        VALUES (%s, %s, %s, %s, %s, %s);
+    """
+    cursor.execute(sql, (str(evaluation_id), str(experiment_id), result['evaluation_type'], result['metric_name'], result['metric_value'], result.get('weights')))
     conn.commit()
 
 def main():
     try:
         conn, cursor = connect_to_db()
+        offset = 0
+        processed_count = 0
+        all_experiment_ids = set()
 
-        # Fetch experiments one by one
-        cursor.execute(f"SELECT * FROM {EXPERIMENTS_TABLE} LIMIT 2000")
+        # Get all existing experiment_ids in the evaluation table to avoid duplicates
+        cursor.execute(f"SELECT DISTINCT experiment_id FROM {EVALUATION_TABLE}")
+        existing_evaluated_ids = {row[0] for row in cursor.fetchall()}
 
         while True:
-            row = cursor.fetchone()
-            if row is None:
+            cursor.execute(f"""
+            SELECT experiment_id, dataset_name, random_seed, test_embeddings, number_of_classes, corrupted_rows, tag
+            FROM {EXPERIMENTS_TABLE} 
+            ORDER BY experiment_id DESC
+            OFFSET %s LIMIT %s""", (offset, BATCH_SIZE))
+            batch = [dict(zip([column.name for column in cursor.description], row)) for row in cursor.fetchall()]
+
+            if not batch:
                 break
 
-            experiment = dict(zip([column.name for column in cursor.description], row))
-            results = evaluate_embeddings(experiment)
-            if results:
-                store_evaluation_metrics(conn, results, experiment['experiment_id'])
-                print(f"Evaluated experiment {experiment['experiment_id']}")
+            for experiment in batch:
+                experiment_id = experiment['experiment_id']
+                if experiment_id not in existing_evaluated_ids and experiment_id not in all_experiment_ids:
+                    results = evaluate_embeddings(experiment)
+                    if results:
+                        for result in results:
+                            store_evaluation_metrics(conn, result, experiment_id)
+                        all_experiment_ids.add(experiment_id)
+                        print(f"Evaluated experiment {experiment_id}")
+                    processed_count += 1
+                else:
+                    print(f"Skipping already evaluated experiment {experiment_id}")
+
+            offset += BATCH_SIZE
 
         cursor.close()
         conn.close()
-        print("Evaluation process completed.")
+        print(f"Evaluation process completed. Processed {processed_count} new experiments.")
 
     except psycopg2.Error as e:
         print(f"Error connecting to or querying the database: {e}")
     except Exception as e:
-        print(f"An unexpected error occurred: {e}")
-
-    except psycopg2.Error as e:
-        print(f"Error connecting to or querying the database: {e}")
-    except Exception as e:
-        print(f"An unexpected error occurred: {e}")
+        print(f"An unexpected error occurred: {str(e)}")
 
 if __name__ == "__main__":
     main()
